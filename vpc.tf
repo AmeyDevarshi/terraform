29802a727507adab18ea45ed8805cc9efd80d3f4
AmeyDevarshi <67369511+AmeyDevarshi@users.noreply.github.com>
2022-01-21 08:48:59 +0530

    Revert "Update README.md"

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
-resource "google_compute_network" "vpc_network1" {
+resource "google_compute_network" "vpc_network" {
   project                 = "probable-skill-338014"
-  name                    = "vpc-network1"
+  name                    = "vpc-network"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
