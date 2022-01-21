2e84788a7004900235cc303f0190bfccb914a055
AmeyDevarshi <67369511+AmeyDevarshi@users.noreply.github.com>
2022-01-21 08:46:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
-resource "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc_network1" {
   project                 = "probable-skill-338014"
-  name                    = "vpc-network"
+  name                    = "vpc-network1"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
